c01e1a2e191878af7677f2bffe24e67dc1b0de02
suzanadeazevedo <suzanadeazevedo@hotmail.com>
2022-08-02 12:50:35 -0300

    V.01

---
+++ src/Boletim.java
@@ -0,0 +1,29 @@
+public class Boletim {
+
+    float nota;
+    int frequencia;
+    String status;
+
+    public void inserir_nota(float nota){
+
+    }
+    public void excluir_nota(){
+
+    }
+
+    public void alterar_nota(float nova_nota){
+
+    }
+
+    public void inserir_frequencia (boolean freq){
+
+    }
+
+    public void alterar_status(){
+
+    }
+
+    public void calcular_media(){
+
+    }
+}
---
+++ src/Faculdade.java
@@ -0,0 +1,17 @@
+public class Faculdade {
+    public static void main(String[] args) {
+
+        Boletim B1 = new Boletim();
+        Boletim B2 = new Boletim();
+
+        B1.inserir_nota(10);
+        B1.inserir_frequencia(true);
+        B1.alterar_status();
+
+        B2.nota = 9;
+        B2.frequencia = 85;
+        B2.status = "Aprovado";
+
+
+    }
+}
